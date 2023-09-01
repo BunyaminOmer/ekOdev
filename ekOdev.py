3a3f9e28727063df46cd08c4a422a3da6bc9834f
BunyaminOmer <bunyaminomerkarakus@protonmail.com>
2023-09-01 19:34:12 +0300

    first commit

---
+++ ekOdev.py
@@ -0,0 +1,609 @@
+# 1
+
+class A:
+   def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+class B:
+   bilgi = "yok"
+
+class C(A, B):
+   pass
+
+# 2
+
+class D:
+    def __init__(self, d, e):
+        self.d = d
+        self.e = e
+
+class E:
+    yaz = "Dene bulursun:)"
+
+class F(D, E):
+   pass
+
+# 3
+
+class G:
+    def __init__(self, H, I):
+        self.H = H
+        self.I = I
+
+class H:
+    yazma = "YHD"
+
+class I(G,H):
+    pass
+
+# 4
+
+class J:
+    def __init__(self, k, l):
+        self.k = k
+        self.l = l
+
+class K:
+    cajio = 3
+
+class L(K, J):
+    pass
+
+# 5
+
+class M:
+    def __init__(self, n, o):
+        self.n = n
+        self.o = o
+
+class N:
+    vshi = 562
+
+class O(M,N):
+    pass
+
+# 6
+
+class P:
+    def __init__(self, r, s):
+        self.r = r
+        self.s = s
+
+class R:
+    deda = 72
+
+class S(P,R):
+    pass
+
+
+# 7
+
+class T:
+    def __init__(self, u, v):
+        self.u = u
+        self.v = v
+
+class U:
+    gahi = 901
+
+class V(T,U):
+    pass
+
+# 8
+
+class Y:
+    def __init__(self, z, ç):
+        self.z = z
+        self.ç = ç
+
+class Z:
+    fja = 5.2
+
+class Ç(Y,Z):
+    pass
+
+# 9
+
+class Ğ:
+    def __init__(self, i, j):
+        self.i = i
+        self.j = j
+
+class İ:
+    adsa = 51663781
+
+class J(Ğ, İ):
+    pass
+
+# 10
+class AA:
+    def __init__(self, BB, CC):
+        self.BB = BB
+        self.CC = CC
+
+class BB:
+    dd = "wksn"
+
+class CC(AA, BB):
+    pass
+
+# 11
+class DD:
+    def __init__(self, EE, FF):
+        self.EE = EE
+        self.FF = FF
+
+class EE:
+    gg = "Dalgınım"
+
+class FF(DD, EE):
+    pass
+
+# 12
+class GG:
+    def __init__(self, HH, II):
+        self.HH = HH
+        self.II = II
+
+class HH:
+    jj = 87
+
+class II(GG, HH):
+    pass
+
+# 13
+class JJ:
+    def __init__(self, KK, LL):
+        self.KK = KK
+        self.LL = LL
+
+class KK:
+    mm = 69
+
+class LL(JJ, KK):
+    pass
+
+# 14
+class MM:
+    def __init__(self, NN, OO):
+        self.NN = NN
+        self.OO = OO
+
+class NN:
+    pp = "0a3k"
+
+class OO(MM, NN):
+    pass
+
+# 15
+class PP:
+    def __init__(self, QQ, RR):
+        self.QQ = QQ
+        self.RR = RR
+
+class QQ:
+    ss = 7.3
+
+class RR(PP, QQ):
+    pass
+
+# 16
+class TT:
+    def __init__(self, UU, VV):
+        self.UU = UU
+        self.VV = VV
+
+class UU:
+    ww = "tskm"
+
+class VV(TT, UU):
+    pass
+
+# 17
+class YY:
+    def __init__(self, ZZ, AA):
+        self.ZZ = ZZ
+        self.AA = AA
+
+class ZZ:
+    bb = 45
+
+class AA(YY, ZZ):
+    pass
+
+# 18
+class BB:
+    def __init__(self, CC, DD):
+        self.CC = CC
+        self.DD = DD
+
+class CC:
+    ee = "4n8l"
+
+class DD(BB, CC):
+    pass
+
+# 19
+class EE:
+    def __init__(self, FF, GG):
+        self.FF = FF
+        self.GG = GG
+
+class FF:
+    hh = 12.6
+
+class GG(EE, FF):
+    pass
+
+# 20
+class İI:
+    def __init__(self, ÖÖ, ÜÜ):
+        self.ÖÖ = ÖÖ
+        self.ÜÜ = ÜÜ
+
+class ÖÖ:
+    çç = "BZM"
+
+class ÜÜ(İI, ÖÖ):
+    pass
+
+# 21
+class ÇÇ:
+    def __init__(self, II, JJ):
+        self.II = II
+        self.JJ = JJ
+
+class II:
+    kk = 56
+
+class JJ(ÇÇ, II):
+    pass
+
+# 22
+class KK:
+    def __init__(self, LL, MM):
+        self.LL = LL
+        self.MM = MM
+
+class LL:
+    nn = "kaka"
+
+class MM(KK, LL):
+    pass
+
+# 23
+class NN:
+    def __init__(self, OO, PP):
+        self.OO = OO
+        self.PP = PP
+
+class OO:
+    qq = 0.25
+
+class PP(NN, OO):
+    pass
+
+# 24
+class RR:
+    def __init__(self, SS, TT):
+        self.SS = SS
+        self.TT = TT
+
+class SS:
+    uu = "ygyz"
+
+class TT(RR, SS):
+    pass
+
+# 25
+class ÜÜ:
+    def __init__(self, ÜÖ, ÇÇ):
+        self.ÜÖ = ÜÖ
+        self.ÇÇ = ÇÇ
+
+class ÜÖ:
+    üi = 8
+
+class ÇÇ(ÜÜ, ÜÖ):
+    pass
+
+# 26
+class XX:
+    def __init__(self, YY, ZZ):
+        self.YY = YY
+        self.ZZ = ZZ
+
+class YY:
+    öö = "qwe"
+
+class ZZ(XX, YY):
+    pass
+
+# 27
+class AAĞ:
+    def __init__(self, BBİ, CCJ):
+        self.BBİ = BBİ
+        self.CCJ = CCJ
+
+class BBİ:
+    ğöğ = "6wf"
+
+class CCJ(AAĞ, BBİ):
+    pass
+
+# 28
+class DDK:
+    def __init__(self, EEK, FFL):
+        self.EEK = EEK
+        self.FFL = FFL
+
+class EEK:
+    mşm = 9.7
+
+class FFL(DDK, EEK):
+    pass
+
+# 29
+class GGİ:
+    def __init__(self, HHÖ, İİÇ):
+        self.HHÖ = HHÖ
+        self.İİÇ = İİÇ
+
+class HHÖ:
+    özl = "bnj"
+
+class İİÇ(GGİ, HHÖ):
+    pass
+
+# 30
+class JJL:
+    def __init__(self, KKÖ, LLM):
+        self.KKÖ = KKÖ
+        self.LLM = LLM
+
+class KKÖ:
+    bkh = 61
+
+class LLM(JJL, KKÖ):
+    pass
+
+# 31
+class MMM:
+    def __init__(self, NNT, OOV):
+        self.NNT = NNT
+        self.OOV = OOV
+
+class NNT:
+    pır = 8.14
+
+class OOV(MMM, NNT):
+    pass
+
+# 32
+class PPÖ:
+    def __init__(self, RRÜ, SST):
+        self.RRÜ = RRÜ
+        self.SST = SST
+
+class RRÜ:
+    ttğ = "hjm"
+
+class SST(PPÖ, RRÜ):
+    pass
+
+# 33
+class TUÜ:
+    def __init__(self, UVÇ, ÜWJ):
+        self.UVÇ = UVÇ
+        self.ÜWJ = ÜWJ
+
+class UVÇ:
+    üjs = 49
+
+class ÜWJ(TUÜ, UVÇ):
+    pass
+
+# 34
+class ÜXK:
+    def __init__(self, YKL, ZLİ):
+        self.YKL = YKL
+        self.ZLİ = ZLİ
+
+class YKL:
+    müy = "wqr"
+
+class ZLİ(ÜXK, YKL):
+    pass
+
+# 35
+class AAMA:
+    def __init__(self, AANA, AAOA):
+        self.AANA = AANA
+        self.AAOA = AAOA
+
+class AANA:
+    aap = 6.3
+
+class AAOA(AAMA, AANA):
+    pass
+
+# 36
+class ABAÖ:
+    def __init__(self, ABÜÇ, ACAG):
+        self.ABÜÇ = ABÜÇ
+        self.ACAG = ACAG
+
+class ABÜÇ:
+    adi = "pvt"
+
+class ACAG(ABAÖ, ABÜÇ):
+    pass
+
+# 37
+class AHA:
+    def __init__(self, AJC, AKJ):
+        self.AJC = AJC
+        self.AKJ = AKJ
+
+class AJC:
+    ada = 81
+
+class AKJ(AHA, AJC):
+    pass
+
+# 38
+class AQA:
+    def __init__(self, ASA, ATA):
+        self.ASA = ASA
+        self.ATA = ATA
+
+class ASA:
+    aua = "xyz"
+
+class ATA(AQA, ASA):
+    pass
+
+# 39
+class AUA:
+    def __init__(self, AVA, AWA):
+        self.AVA = AVA
+        self.AWA = AWA
+
+class AVA:
+    axa = 9.5
+
+class AWA(AUA, AVA):
+    pass
+
+# 40
+class AXA:
+    def __init__(self, AYA, AZA):
+        self.AYA = AYA
+        self.AZA = AZA
+
+class AYA:
+    aza = "aaa"
+
+class AZA(AXA, AYA):
+    pass
+
+# 41
+class ÇAÇ:
+    def __init__(self, ÇEÇ, ÇİÇ):
+        self.ÇEÇ = ÇEÇ
+        self.ÇİÇ = ÇİÇ
+
+class ÇEÇ:
+    çjç = 31
+
+class ÇİÇ(ÇAÇ, ÇEÇ):
+    pass
+
+# 42
+class YTY:
+    def __init__(self, TYT, UYU):
+        self.TYT = TYT
+        self.UYU = UYU
+
+class TYT:
+    TYU = "uia"
+
+class UYU(YTY, TYT):
+    pass
+
+# 43
+class İYİ:
+    def __init__(self, ÜÜÜ, AAĞ):
+        self.ÜÜÜ = ÜÜÜ
+        self.AAĞ = AAĞ
+
+class ÜÜÜ:
+    İİİ = 4
+
+class AAĞ(İYİ, ÜÜÜ):
+    pass
+
+# 44
+class ABB:
+    def __init__(self, ACC, ADD):
+        self.ACC = ACC
+        self.ADD = ADD
+
+class ACC:
+    BİL = "çpü"
+
+class ADD(ABB, ACC):
+    pass
+
+# 45
+class YTY:
+    def __init__(self, TYT, UYU):
+        self.TYT = TYT
+        self.UYU = UYU
+
+class TYT:
+    TYU = "uia"
+
+class UYU(YTY, TYT):
+    pass
+
+# 46
+class EEÜ:
+    def __init__(self, EEÖ, ÜÜÇ):
+        self.EEÖ = EEÖ
+        self.ÜÜÇ = ÜÜÇ
+
+class EEÖ:
+    EEÇ = 89
+
+class ÜÜÇ(EEÜ, EEÖ):
+    pass
+
+# 47
+class GÖÖ:
+    def __init__(self, GOÇ, GĞÇ):
+        self.GOÇ = GOÇ
+        self.GĞÇ = GĞÇ
+
+class GOÇ:
+    GÖÖ = "dvn"
+
+class GĞÇ(GÖÖ, GOÇ):
+    pass
+
+# 48
+class GÖL:
+    def __init__(self, GOÖ, GÜİ):
+        self.GOÖ = GOÖ
+        self.GÜİ = GÜİ
+
+class GOÖ:
+    GÖŞ = 2.5
+
+class GÜİ(GÖL, GOÖ):
+    pass
+
+# 49
+class KÜY:
+    def __init__(self, KYÜ, KİÇ):
+        self.KYÜ = KYÜ
+        self.KİÇ = KİÇ
+
+class KYÜ:
+    KÜÖ = "owq"
+
+class KİÇ(KÜY, KYÜ):
+    pass
+
+# 50
+class GÖÜ:
+    def __init__(self, GYÜ, GZÖ):
+        self.GYÜ = GYÜ
+        self.GZÖ = GZÖ
+
+class GYÜ:
+    GZİ = 18
+
+class GZÖ(GÖÜ, GYÜ):
+    pass
